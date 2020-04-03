f5388128c2c41d4fb48c553590fefcaf3fd30619
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2020-04-02 20:01:01 -0400

    Add rest of bitwsie operator test.

--- bitwise_ops/bitwise_ops.c
+++ bitwise_ops/bitwise_ops.c
@@ -12,5 +12,21 @@ int main() {
     printf("Got the bitwise OR right.\n");
   }
 
+  if((a ^ b) == 0b10010110) {
+    printf("Got the bitwise XOR right.\n");
+  }
+
+  if(~a == 0b11111111111111111111111111000011) {
+    printf("Got the bitwsie NEGATION right.\n");
+  }
+
+  if((a << 2) == 0b11110000) {
+    printf("Got the bitwise LEFT SHIFT right.\n");
+  }
+
+  if((a >> 2) == 0b00001111) {
+    printf("Got the bitwise RIGHT SHIFT right.\n");
+  }
+
   return 0;
 }
