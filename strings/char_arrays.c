7fef06475142b5ba53e06f153bcf09f3a6397a85
slavnikov <dmitry.slavnikov@gmail.com>
2020-08-25 17:44:20 -0400

    Implement my_strcat() and my_strcmp().

--- strings/char_arrays.c
+++ strings/char_arrays.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 int my_strlen(char string[]);
+void my_strcat(char dst_str[], char src_str[]);
+bool my_strcmp(char str1[], char str2[]);
 
 int main(int argc, char *argv[]) {
   char *input = argv[1];
@@ -10,6 +13,7 @@ int main(int argc, char *argv[]) {
     return 0;
   }
 
+  printf("\n");
   printf("The input is \"%s\".\n", input);
 
   int str_length = strlen(input);
@@ -19,6 +23,25 @@ int main(int argc, char *argv[]) {
   printf("My answer is: %d\n", myf_length);
   printf("The function 'my_strlen' %s\n", str_length == myf_length ? "worked!" : "did not work :(");
 
+  char dst_str1[13] = "Hello";
+  char dst_str2[13] = "Hello";
+  char src_str[] = " World!";
+  strcat(dst_str1, src_str);
+  my_strcat(dst_str2, src_str);
+
+  printf("\n");
+  printf("Standard concat output is: %s\n", dst_str1);
+  printf("My concat output is: %s\n", dst_str2);
+  printf("The function 'my_strcat' %s\n", strcmp(dst_str1, dst_str2) == 0 ? "worked!" : "did not work :(");
+
+  char foo1[] = "foo";
+  char foo2[] = "foo";
+  char bar[] = "bar";
+  bool comp3 = my_strcmp(foo1, foo2);
+  bool comp4 = my_strcmp(foo1, bar);
+
+  printf("\n");
+  printf("The function 'my_strcomp' %s\n", comp3 && !comp4 ? "worked!" : "did not work :(");
 
   return 0;
 }
@@ -31,4 +54,30 @@ int my_strlen(char string[]) {
   }
 
   return length;
+}
+
+void my_strcat(char dst_str[], char src_str[]) {
+  int dst_idx = my_strlen(dst_str);
+  int src_idx = 0;
+
+  while(src_str[src_idx] != '\0') {
+    dst_str[dst_idx] = src_str[src_idx];
+    dst_idx++;
+    src_idx++;
+  }
+  dst_str[dst_idx] = '\0';
+}
+
+bool my_strcmp(char str1[], char str2[]) {
+  int idx = 0;
+
+  while(true) {
+    if(str1[idx] != str2[idx]) {
+      return false;
+    } else if (str1[idx] == '\0') {
+      return true;
+    } else {
+      idx++;
+    }
+  }
 }
