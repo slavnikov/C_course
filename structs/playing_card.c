543a6eb326d65017121e3476ac132b9dd0ed3c68
slavnikov <dmitry.slavnikov@gmail.com>
2021-02-09 17:23:42 -0500

    Implement more card methods.

--- structs/playing_card.c
+++ structs/playing_card.c
@@ -15,20 +15,28 @@ struct PlayingCard {
 };
 
 struct Deck {
-  struct PlayingCard cards[52];
+  struct PlayingCard *cards[52];
 };
 
 struct CardHand {
   unsigned char card_count;
-  struct PlayingCard *cards;
+  struct PlayingCard **cards;
 };
 
-void showCard(struct PlayingCard *cardPtr);
 char* suitToString(enum Suit suit);
+void fillDeck(struct Deck *deck);
+void showCard(struct PlayingCard *cardPtr);
+void showHand(struct CardHand *card_handPtr);
+void showDeck(struct Deck *deck);
 
 int main(int argc, char const *argv[]) {
   struct PlayingCard a_card;
+  struct PlayingCard b_card = {HEARTS, '5'};
+  struct CardHand a_hand;
+  struct Deck a_deck;
   struct PlayingCard *a_cardPtr;
+  struct PlayingCard three_cards[3];
+  struct PlayingCard five_cards[5] = {{SPADES, 'A'}, {HEARTS, 'Q'}, [3] = {CLUBS, '6'}}; // SETS THE FIRST, SECOND, AND FOURTH THINGS IN THE ARRAY
 
   a_card.suit = DIAMONDS;
   a_card.value = '5';
@@ -36,43 +44,91 @@ int main(int argc, char const *argv[]) {
   a_cardPtr = &a_card;
   showCard(a_cardPtr);
 
-  struct PlayingCard three_cards[3];
-  struct PlayingCard five_cards[5] = {{SPADES, 'A'}, {HEARTS, 'Q'}, [3] = {CLUBS, '6'}}; // SETS THE FIRST, SECOND, AND FOURTH THINGS IN THE ARRAY
+  a_hand.card_count = 2;
+  a_hand.cards = malloc(2 * sizeof(struct PlayingCard));
+  a_hand.cards[0] = a_cardPtr;
+  a_hand.cards[1] = &b_card;
+  showHand(&a_hand);
 
+  fillDeck(&a_deck);
+  showDeck(&a_deck);
 
+  free(a_hand.cards);
+  for (short i = 0; i < 52; i++) {
+    free(a_deck.cards[i]);
+  }
   return 0;
+}
+
+void showHand(struct CardHand *card_handPtr) {
+  for (unsigned char i = 0; i < card_handPtr->card_count; i++) {
+    showCard(card_handPtr->cards[i]);
+  }
+}
+
+void showDeck(struct Deck *deckPtr) {
+  for (unsigned char i = 0; i < 52; i++) {
+    showCard(deckPtr->cards[i]);
+  }
 }
 
 void showCard(struct PlayingCard *cardPtr) {
   char *suit_string = suitToString((*cardPtr).suit);    //(*ptr).attr is one way to travere a sturct pointer to an attribute value
 
-  printf("%c of %s\n", cardPtr->value, suit_string);  //ptr->attr is another way to traverse a struct ointer to an attribute value
-  free(suit_string);
+  printf("%c of %s\n", cardPtr->value, suit_string);  //ptr->attr is another way to traverse a struct pointer to an attribute value
+}
+
+void fillDeck(struct Deck *deck) {
+  unsigned char card_idx = 0;
+
+  for (enum Suit suit = SPADES; suit <= DIAMONDS; suit++) {
+    for (short val = 2; val <= 14; val++) {
+      struct PlayingCard card_to_add = {suit};
+      struct PlayingCard *card_to_add_ptr = malloc(sizeof(struct PlayingCard));
+
+      switch (val) {
+        case 10:
+          card_to_add.value = 'X';
+          break;
+        case 11:
+          card_to_add.value = 'J';
+          break;
+        case 12:
+          card_to_add.value = 'Q';
+          break;
+        case 13:
+          card_to_add.value = 'K';
+          break;
+        case 14:
+          card_to_add.value = 'A';
+          break;
+        default:
+          card_to_add.value = (char) val + '0';
+      }
+      *card_to_add_ptr = card_to_add;
+      deck->cards[card_idx] = card_to_add_ptr;
+      card_idx++;
+    }
+  }
 }
 
 char* suitToString(enum Suit suit) {
-  // char *suit_string = calloc(9, sizeof(char));
   char *suit_string;
 
   switch (suit) {
     case SPADES:
-    // strcpy(suit_string, "SPADES");
       suit_string = "SPADES";
       break;
     case HEARTS:
-    // strcpy(suit_string, "HEARTS");
       suit_string = "HEARTS";
       break;
     case CLUBS:
-    // strcpy(suit_string, "CLUBS");
       suit_string = "CLUBS";
       break;
     case DIAMONDS:
-    // strcpy(suit_string, "DIAMONDS");
       suit_string = "DIAMONDS";
       break;
     default:
-      // strcpy(suit_string, "???");
       suit_string = "???";
       break;
   }
